d3a71f15ca33db245895c9225e31f88005dbfa41
Nicolas Barbotin <nicolas@barbot.in>
2026-08-09 18:05:05 +0930

    Base path support

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  base: process.env["BASE_PATH"] || "/",
   test: {
     include: ['tests/**/*.test.ts'],
     environment: 'node',
